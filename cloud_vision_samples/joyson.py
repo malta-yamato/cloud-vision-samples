3403535c104ba2f7a51c132d0366dabd245a74eb
Yamato Malta <malta.yamato@gmail.com>
2018-12-09 19:52:18 +0900

    refactoring

--- cloud_vision_samples/joyson.py
+++ cloud_vision_samples/joyson.py
@@ -3,10 +3,14 @@ from enum import Enum
 
 class Joyson:
     def __init__(self, action=None, index_key_filter=None):
+        if not callable(action):
+            raise ValueError("action must be callable.")
         if index_key_filter is not None and type(index_key_filter) is not list:
-            raise ValueError("index_key_filter must be list")
+            raise ValueError("index_key_filter must be list.")
         self._action = action
         self._filter = index_key_filter
+        self._has_action = action is not None
+        self._has_filter = index_key_filter is not None
         self._depth = 0
         self._depth_filtered = 0
 
@@ -16,55 +20,79 @@ class Joyson:
         KEY = 2
         VALUE = 3
 
+    class Termination(Enum):
+        NONE = 0
+        RETURN = 1
+        CONTINUE = 2
+        BREAK = 3
+
     @staticmethod
-    def accept(json_element, jason):
-        jason.visit(json_element)
+    def accept(json_element, joyson):
+        joyson.visit(json_element)
 
     def visit(self, element):
         filtered = self._check_filter()
-        self._depth += 1
-        if filtered:
-            self._depth_filtered += 1
-        if filtered and self._action(Joyson.Phase.WHOLE, self._depth, self._depth_filtered, element):
-            return
+        self._change_depth(filtered, 1)
+        #
+        can_action = self._has_action and filtered
+        action = self._action
+        depth = self._depth
+        depth_filtered = self._depth_filtered
+        if can_action:
+            ret = action(Joyson.Phase.WHOLE, depth, depth_filtered, element)
+            if ret == Joyson.Termination.RETURN:
+                self._change_depth(filtered, -1)
+                return
+        #
         type_elm = type(element)
         if type_elm is dict:
             for key in element:
-                ret = filtered and self._action(Joyson.Phase.KEY, self._depth, self._depth_filtered, key)
+                termination = Joyson.Termination.NONE
+                if can_action:
+                    termination = action(Joyson.Phase.KEY, depth, depth_filtered, key)
+                if termination == Joyson.Termination.CONTINUE:
+                    continue
                 self._accept(element, key)
-                if ret:
+                if termination == Joyson.Termination.BREAK:
                     break
         elif type_elm is list:
             for idx, _ in enumerate(element):
-                ret = filtered and self._action(Joyson.Phase.INDEX, self._depth, self._depth_filtered, idx)
+                termination = Joyson.Termination.NONE
+                if can_action:
+                    termination = can_action and action(Joyson.Phase.INDEX, depth, depth_filtered, idx)
+                if termination == Joyson.Termination.CONTINUE:
+                    continue
                 self._accept(element, idx)
-                if ret:
+                if termination == Joyson.Termination.BREAK:
                     break
         else:
-            if self._action is not None and filtered:
-                filtered and self._action(Joyson.Phase.VALUE, self._depth, self._depth_filtered, element)
-        self._depth -= 1
-        if filtered:
-            self._depth_filtered -= 1
+            can_action and self._action(Joyson.Phase.VALUE, depth, depth_filtered, element)
+        #
+        self._change_depth(filtered, -1)
 
     def _accept(self, element, index_or_key):
         tmp = self._pop_filter() if self._peek_filter() == index_or_key else None
         Joyson.accept(element[index_or_key], self)
         self._push_filter(tmp)
 
+    def _change_depth(self, filtered, num):
+        self._depth += num
+        if filtered:
+            self._depth_filtered += num
+
     def _check_filter(self):
-        return self._filter is None or len(self._filter) == 0
+        return not self._has_filter or len(self._filter) == 0
 
     def _peek_filter(self):
-        if self._filter is not None and len(self._filter) > 0:
+        if self._has_filter and len(self._filter) > 0:
             return self._filter[-1]
         return None
 
     def _pop_filter(self):
-        if self._filter is not None and len(self._filter) > 0:
+        if self._has_filter and len(self._filter) > 0:
             return self._filter.pop()
         return None
 
     def _push_filter(self, index_or_key):
-        if self._filter is not None and index_or_key is not None:
+        if self._has_filter and index_or_key is not None:
             self._filter.append(index_or_key)
