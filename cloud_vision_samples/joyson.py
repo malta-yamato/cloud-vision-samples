a271a5ee2c24aad6a54484d44336400a4a32a4d4
Yamato Malta <malta.yamato@gmail.com>
2018-12-10 00:35:42 +0900

    early enhancement

--- cloud_vision_samples/joyson.py
+++ cloud_vision_samples/joyson.py
@@ -2,23 +2,31 @@ from enum import Enum
 
 
 class Joyson:
-    def __init__(self, action=None, index_key_filter=None):
-        if not callable(action):
-            raise ValueError("action must be callable.")
-        if index_key_filter is not None and type(index_key_filter) is not list:
+    def __init__(self, action_object=None, key_index_filter=None):
+        if not (callable(action_object) or isinstance(action_object, Joyson.Action)):
+            raise ValueError("action must be callable or Jayson.Action class")
+        if key_index_filter is not None and type(key_index_filter) is not list:
             raise ValueError("index_key_filter must be list.")
-        self._action = action
-        self._filter = index_key_filter
-        self._has_action = action is not None
-        self._has_filter = index_key_filter is not None
+        self._action_object = action_object
+        self._filter = key_index_filter
+        self._has_action = action_object is not None
+        self._has_filter = key_index_filter is not None
         self._depth = 0
         self._depth_filtered = 0
 
+    class Action:
+        def action(self, phase, depth, depth_filtered, element):
+            pass
+
     class Phase(Enum):
         WHOLE = 0
-        INDEX = 1
-        KEY = 2
-        VALUE = 3
+        IN_DICT = 1
+        OUT_DICT = 2
+        IN_LIST = 3
+        OUT_LIST = 4
+        KEY = 5
+        INDEX = 6
+        VALUE = 7
 
     class Termination(Enum):
         NONE = 0
@@ -35,45 +43,53 @@ class Joyson:
         self._change_depth(filtered, 1)
         #
         can_action = self._has_action and filtered
-        action = self._action
         depth = self._depth
         depth_filtered = self._depth_filtered
-        if can_action:
-            ret = action(Joyson.Phase.WHOLE, depth, depth_filtered, element)
-            if ret == Joyson.Termination.RETURN:
-                self._change_depth(filtered, -1)
-                return
+        if self._action(can_action, Joyson.Phase.WHOLE, depth, depth_filtered, element) == Joyson.Termination.RETURN:
+            self._change_depth(filtered, -1)
+            return
         #
         type_elm = type(element)
         if type_elm is dict:
+            self._action(can_action, Joyson.Phase.IN_DICT, depth, depth_filtered, element)
             for key in element:
-                termination = Joyson.Termination.NONE
-                if can_action:
-                    termination = action(Joyson.Phase.KEY, depth, depth_filtered, key)
+                termination = self._action(can_action, Joyson.Phase.KEY, depth, depth_filtered, key)
                 if termination == Joyson.Termination.CONTINUE:
                     continue
                 self._accept(element, key)
                 if termination == Joyson.Termination.BREAK:
                     break
+            self._action(can_action, Joyson.Phase.OUT_DICT, depth, depth_filtered, element)
         elif type_elm is list:
+            self._action(can_action, Joyson.Phase.IN_LIST, depth, depth_filtered, element)
             for idx, _ in enumerate(element):
-                termination = Joyson.Termination.NONE
-                if can_action:
-                    termination = can_action and action(Joyson.Phase.INDEX, depth, depth_filtered, idx)
+                termination = self._action(can_action, Joyson.Phase.INDEX, depth, depth_filtered, idx)
                 if termination == Joyson.Termination.CONTINUE:
                     continue
                 self._accept(element, idx)
                 if termination == Joyson.Termination.BREAK:
                     break
+            self._action(can_action, Joyson.Phase.OUT_LIST, depth, depth_filtered, element)
         else:
-            can_action and self._action(Joyson.Phase.VALUE, depth, depth_filtered, element)
+            self._action(can_action, Joyson.Phase.VALUE, depth, depth_filtered, element)
         #
         self._change_depth(filtered, -1)
 
-    def _accept(self, element, index_or_key):
-        tmp = self._pop_filter() if self._peek_filter() == index_or_key else None
-        Joyson.accept(element[index_or_key], self)
+    def _accept(self, element, key_or_index):
+        tmp = self._pop_filter() if self._peek_filter() == key_or_index else None
+        Joyson.accept(element[key_or_index], self)
         self._push_filter(tmp)
+
+    def _action(self, can_action, phase, depth, depth_filtered, element):
+        if not can_action:
+            return Joyson.Termination.NONE
+        action = self._action_object
+        if callable(action):
+            return action(phase, depth, depth_filtered, element)
+        elif isinstance(action, Joyson.Action):
+            return action.action(phase, depth, depth_filtered, element)
+        else:
+            raise ValueError("Invalid action")
 
     def _change_depth(self, filtered, num):
         self._depth += num
